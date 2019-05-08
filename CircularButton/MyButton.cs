fbe282b765ef698b517961a68960c7c1cec0db04
stepmania003 <stepmania003@gmail.com>
2019-05-08 10:28:53 +0800

    加入專案檔案。

---
+++ CircularButton/MyButton.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CircularButton
+{
+    public class MyButton : Control
+    {
+        //設定Button 形狀屬性 
+        public enum Fillstyle
+        {
+            Ellipse,
+            Rectangle
+        }
+        private Fillstyle _mystyle;
+        private Color _color;
+        private Color _顏色;
+        [Category("我的自訂選項"), Description("........")]
+        public Color 顏色
+        {
+            get
+            { return _顏色; }
+            set
+            {
+                _顏色 = value;
+                this.Invalidate();
+            }
+        }
+
+        [Category("我的自訂選項"), Description("........")]
+        public Fillstyle 形狀
+        {
+            get
+            { return _mystyle; }
+            set
+            { _mystyle = value; }
+        }
+        [Category("我的自訂選項"), Description("........")]
+        public Color 點擊顏色 { get; set; }
+
+        public MyButton()
+        {
+            this.Width = 200;
+            this.Height = 200;
+            this.形狀 = Fillstyle.Ellipse;
+            base.Font = new Font("微軟正黑體", 9, FontStyle.Bold);
+            this.MouseDown += delegate { myPen = new Pen(Color.Black, 3); };
+            //this.Paint += delegate { this.dracir(); };
+        }
+        Pen myPen = new Pen(Color.Black, 2);
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Brush brush = new SolidBrush(this._顏色);
+            switch (形狀)
+            {
+                case Fillstyle.Ellipse:
+                    e.Graphics.ScaleTransform(0.99f, 0.99f);
+                    e.Graphics.FillEllipse(brush, 2, 2, this.Width , this.Height);
+                    e.Graphics.DrawEllipse(myPen, new Rectangle(2, 2, this.Width - 3, this.Height - 3));
+                    break;
+                case Fillstyle.Rectangle:
+                    e.Graphics.FillRectangle(brush, 2, 2, this.Width, this.Height);
+                    e.Graphics.DrawRectangle(myPen, new Rectangle(2, 2, this.Width - 3, this.Height - 3));
+                    break;
+            }
+            TextFormatFlags flags = TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter;
+            TextRenderer.DrawText(e.Graphics, this.Text, this.Font, new Point(this.Width + 3, this.Height / 2), this.ForeColor, flags);
+            base.OnPaint(e);
+        }
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            this._color = this._顏色;
+            base.OnMouseDown(e);
+            this.顏色 = 點擊顏色;
+            this.Invalidate();
+        }
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            this.顏色 = this._color;
+            myPen = new Pen(Color.Black, 2);
+            this.Invalidate();
+        }
+    }
+}
---
+++ CircularButton/UserControl2.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace CircularButton
+{
+    partial class UserControl2
+    {
+        /// <summary> 
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 元件設計工具產生的程式碼
+
+        /// <summary> 
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myButton1 = new CircularButton.MyButton();
+            this.SuspendLayout();
+            // 
+            // myButton1
+            // 
+            this.myButton1.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold);
+            this.myButton1.Location = new System.Drawing.Point(148, 55);
+            this.myButton1.Name = "myButton1";
+            this.myButton1.Size = new System.Drawing.Size(200, 200);
+            this.myButton1.TabIndex = 0;
+            this.myButton1.Text = "myButton1";
+            this.myButton1.形狀 = CircularButton.MyButton.Fillstyle.Ellipse;
+            this.myButton1.顏色 = System.Drawing.Color.Empty;
+            this.myButton1.點擊顏色 = System.Drawing.Color.Empty;
+            // 
+            // UserControl2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.myButton1);
+            this.Name = "UserControl2";
+            this.Size = new System.Drawing.Size(516, 395);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MyButton myButton1;
+    }
+}
